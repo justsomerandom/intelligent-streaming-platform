df1057db39ed5dd6ee9c64618e77fb2ec926bc12
justsomerandom <abudib03@gmail.com>
2025-05-15 15:08:56 +0300

    Client Vite init

---
+++ src/client/src/App.tsx
@@ -0,0 +1,13 @@
+// filepath: client/src/App.tsx
+import React from 'react';
+
+const App: React.FC = () => {
+    return (
+        <div>
+            <h1>Welcome to the Intelligent Streaming Platform</h1>
+            <p>This is a simple webpage that will later intake data from other Docker containers.</p>
+        </div>
+    );
+};
+
+export default App;
---
+++ src/client/src/vite-env.d.ts
@@ -0,0 +1,2 @@
+// filepath: client/src/vite-env.d.ts
+/// <reference types="vite/client" />
