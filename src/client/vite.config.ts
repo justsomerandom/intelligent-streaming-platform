24f19f8bf7481631c5b34ae75368f302b0780e54
justsomerandom <abudib03@gmail.com>
2025-05-19 01:21:17 +0300

    Client fixes

--- src/client/vite.config.ts
+++ src/client/vite.config.ts
@@ -4,8 +4,15 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
     port: 5173,
-    open: false,
-    host: '0.0.0.0'
+    strictPort: true,
+    watch: {
+      usePolling: true,
+    },
+    hmr: {
+      host: 'localhost',
+      port: 24678,
+    }
   },
 });
