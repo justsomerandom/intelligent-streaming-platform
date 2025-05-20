de41902606511f191740c4c8b005bc02957c955c
justsomerandom <abudib03@gmail.com>
2025-05-20 22:44:20 +0300

    Removed Docker

--- windows/cam_teardown.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Stopping all ffmpeg streaming processes...
-
-for /f "tokens=2" %%a in ('tasklist ^| findstr ffmpeg.exe') do (
-    echo Terminating PID %%a
-    taskkill /F /PID %%a
-)
-
-echo Done.
